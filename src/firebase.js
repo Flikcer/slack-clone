8c1117c8863e61e96f6cc0067b94fbfc798a6059
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-25 17:57:25 -0400

    connection to firebase successful

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD6WC4qzR3LA2fHXXr3UeKdvECfYSYJcjI",
@@ -9,3 +11,11 @@ const firebaseConfig = {
   messagingSenderId: "1079346783938",
   appId: "1:1079346783938:web:37d5b352efe02eab183d5b",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
